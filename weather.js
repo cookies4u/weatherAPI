ee53cb3371371bb844d0bd41e7a9e228b94aa36e
cookies4u <jcreyes.1018@gmail.com>
2017-07-26 23:23:51 -0700

    back end fully functional

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@
 var lat, lon;
 var tempUnit = 'C';
 var currentTempInCelsius;
-var api = 'https://fcc-weather-api.glitch.me/api/current?' + lat + lon;
+var api = 'https://fcc-weather-api.glitch.me/api/current?';
 // var api = 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139'
 
 $( document ).ready(function(){
@@ -24,17 +24,32 @@ $( document ).ready(function(){
 	}
 	getLocation();
 	*/
+
 	if (navigator.geolocation) {
+
 	    navigator.geolocation.getCurrentPosition(function (position) {
+
 	    	var lat = "lat=" + position.coords.latitude;
 	    	var lon = "lon=" + position.coords.longitude;
+
 	    	console.log(lat, " ", lon);
 	    	getWeather(lat, lon);
-
 	    });
 	} else { 
 	    console.log("Geolocation is not supported by this browser.");
 	}
+
+	$("#tempUnit").click(function() {
+		tempUnit = (tempUnit === 'C' ? 'F' : 'C');
+		console.log(tempUnit);
+		$('#tempUnit').text(tempUnit);
+		if (tempUnit === 'F') {
+			var fahTemp = Math.round(parseInt($("#temp").text()) * 9 / 5 + 32);
+			$("#temp").text(fahTemp + " " + String.fromCharCode(176));
+		} else {
+			$("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
+		}
+	});
 
 });
 
@@ -44,33 +59,66 @@ $( document ).ready(function(){
 
 var getWeather = (lat, lon) => {
 	$.ajax({
-	  url: api,
+	  url: api + lat + '&' + lon,
 	  method: "GET"
 	}).done(function(response) {
 	  console.log(response);
 	  console.log(response.main.temp);
+	  $("#city").text(response.name + ", ");
+      $("#country").text(response.sys.country);
+
+      currentTempInCelsius = response.main.temp;
+      $("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
+      $("#tempunit").text(tempUnit);
+
+      document.getElementById('icon').setAttribute("src", response.weather[0].icon);
+      $("#description").text(response.weather[0].description);
 	});
 }
 
 /*
-{ 
-	"coord": { "lon":159, "lat":35 },
-	"weather":
-		[{ 
-			"id":500, 
-			"main":"Rain", 
-			"description":"light rain", 
-			"icon":"https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F10n.png?1499366021399" 
-		}], 
-	"base":"stations", 
-	"main":{ "temp":22.59, "pressure":1027.45, "humidity":100, "temp_min":22.59, "temp_max":22.59, "sea_level":1027.47, "grnd_level":1027.45 }, 
-	"wind":{ "speed":8.12, "deg":246.503 }, 
-	"rain":{ "3h":0.45 },
-	"clouds":{ "all":92 }, 
-	"dt":1499521932, 
-	"sys":{ "message":0.0034, "sunrise":1499451436, "sunset":1499503246 }, 
-	"id":0, 
-	"name":"", 
-	"cod":200 
+{
+    "coord": {
+        "lon": 139,
+        "lat": 35
+    },
+    "weather": [
+        {
+            "id": 501,
+            "main": "Rain",
+            "description": "moderate rain",
+            "icon": "https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F10n.png?1499366021399"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 24.3,
+        "pressure": 1010.78,
+        "humidity": 100,
+        "temp_min": 24.3,
+        "temp_max": 24.3,
+        "sea_level": 1019.77,
+        "grnd_level": 1010.78
+    },
+    "wind": {
+        "speed": 8.36,
+        "deg": 27.0031
+    },
+    "rain": {
+        "3h": 8.285
+    },
+    "clouds": {
+        "all": 80
+    },
+    "dt": 1501022355,
+    "sys": {
+        "message": 0.0039,
+        "country": "JP",
+        "sunrise": 1500925721,
+        "sunset": 1500976317
+    },
+    "id": 1851632,
+    "name": "Shuzenji",
+    "cod": 200
 }
 */
